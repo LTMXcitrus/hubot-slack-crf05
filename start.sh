5f34df89d84deabcb2996f46aa859a353fe8b48a
matthieu_lemonnier <mlemonnier@voyages-sncf.com>
2017-09-22 10:24:46 +0200

    changed token

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-HUBOT_SLACK_TOKEN=xoxb-244170912705-Tk55j91gwAvKDJkDDlJfNbjv ./bin/hubot --adapter slack
+HUBOT_SLACK_TOKEN=xoxb-244170912705-bb9aK3nhaw2NMNdZ7KgPAViX ./bin/hubot --adapter slack
