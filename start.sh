a3b35a53e01030afd6c2977cad43815ecf5daa09
matthieu_lemonnier <mlemonnier@voyages-sncf.com>
2017-09-22 10:35:05 +0200

    invalid token

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-HUBOT_SLACK_TOKEN=xoxb-244170912705-bb9aK3nhaw2NMNdZ7KgPAViX ./bin/hubot --adapter slack
+HUBOT_SLACK_TOKEN=xoxb-244170912705-3IS6fQiLACmo9skL3McyzcvX ./bin/hubot --adapter slack
