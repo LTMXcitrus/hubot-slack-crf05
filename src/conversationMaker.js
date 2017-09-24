3ec71d3b1792c1342909fdada7ea3d4f9d117a38
LTMXcitrus <mattcitron45140@gmail.com>
2017-09-24 23:57:31 +0200

    added smalltalk intents

--- src/conversationMaker.js
+++ src/conversationMaker.js
@@ -1,41 +1,62 @@
 var gSheetsClient = require('../src/gSheetsClient');
 
-module.exports.getAnswer = function(body, callback){
+module.exports.getAnswer = function (body, callback) {
   var intent = body.intent;
   var entity = body.entities.length > 0
     ? body.retainedQuery.substring(body.entities[0].start, body.entities[0].end)
     : null;
-  if(intent === "hello") {
-    callback(oneOf(["salut toi", "oh, salut !", "Coucou!"]))
-  }
-  else if(intent === "whoareyou"){
-    callback(oneOf(["Je suis le bot du mot !", "Moi ? je suis un bot! et toi ?"]))
-  }
-  else if(intent === "whatabout"){
-    callback("Je regarde ça...");
-    gSheetsClient.retrieveObject("compresses", null, callback);
-  }
-  else if(intent === "whereis"){
-    callback("Je regarde ça...");
-    gSheetsClient.retrieveObject(entity, "Etagere", callback);
-  }
-  else if(intent === "howmany"){
-    callback("Je regarde ça...");
-    gSheetsClient.retrieveObject(entity, "stock", callback);
-  }
-  else if(intent === "expiration"){
-    callback("Je regarde ça...");
-    gSheetsClient.retrieveObject(entity, "expiration date", callback);
-  }
-  else if(intent === "toorder"){
-    callback("Je regarde ça...");
-    gSheetsClient.retrieveObject(entity, "to order", callback);
-  }
-  else {
-    callback("Quelqu'un me parle ?")
+  switch (intent) {
+    case "hello":  {
+      callback(oneOf(["salut toi", "oh, salut !", "Coucou!"]))
+      break;
+    }
+    case "whoareyou": {
+      callback(oneOf(["Je suis le bot du mot !", "Moi ? je suis un bot! et toi ?"]))
+      break;
+    }
+    case "whatabout": {
+      callback("Je regarde ça...");
+      gSheetsClient.retrieveObject("compresses", null, callback);
+      break;
+    }
+    case "whereis": {
+      callback("Je regarde ça...");
+      gSheetsClient.retrieveObject(entity, "Etagere", callback);
+      break;
+    }
+    case "howmany": {
+      callback("Je regarde ça...");
+      gSheetsClient.retrieveObject(entity, "stock", callback);
+      break;
+    }
+    case "expiration": {
+      callback("Je regarde ça...");
+      gSheetsClient.retrieveObject(entity, "expiration date", callback);
+      break;
+    }
+    case "toorder": {
+      callback("Je regarde ça...");
+      gSheetsClient.retrieveObject(entity, "to order", callback);
+      break;
+    }
+    case "joke": {
+      callback("et 1 - 0 pour l'humour!")
+      break;
+    }
+    case "howareyou": {
+      callback(oneOf(["ça va bien, et toi ?", "ça se passe tranquille... :)", "On s'ennuie vite ici, tu sais..."]))
+      break;
+    }
+    case "thankyou": {
+      callback(oneOf(["Y a pas de problème!", "Mais de rien!", "Avec plaisir!"]));
+      break;
+    }
+    default: {
+      callback("Quelqu'un me parle ?")
+    }
   }
 };
 
-var oneOf = function(list) {
+var oneOf = function (list) {
   return list[Math.floor(Math.random() * list.length)];
 };
