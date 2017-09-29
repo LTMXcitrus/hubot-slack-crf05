23922ae803187c43b19d09a9a8616d336da3026d
matthieu_lemonnier <mlemonnier@voyages-sncf.com>
2017-09-29 13:29:55 +0200

    Added tags handling
    Added whosthebest intent

--- src/conversationMaker.js
+++ src/conversationMaker.js
@@ -1,10 +1,14 @@
 var gSheetsClient = require('../src/gSheetsClient');
 
+var TITANE = "vsc:titane";
+var TOPAZE = "vsc:topaze";
+var FORMAT = "vsc:format";
+var ACTION = "vsc:action";
+
 module.exports.getAnswer = function (body, callback) {
   var intent = body.intent;
-  var entity = body.entities.length > 0
-    ? body.retainedQuery.substring(body.entities[0].start, body.entities[0].end)
-    : null;
+  var entityValues = getEntityValues(body.entities, body.retainedQuery);
+  var entityNames = getEntityNames(body.entities);
   switch (intent) {
     case "hello":  {
       callback(oneOf(["salut toi", "oh, salut !", "Coucou!"]))
@@ -21,22 +25,22 @@ module.exports.getAnswer = function (body, callback) {
     }
     case "whereis": {
       callback("Je regarde ça...");
-      gSheetsClient.retrieveObject(entity, "Etagere", callback);
+      gSheetsClient.retrieveObject(entityValues, "Etagere", callback);
       break;
     }
     case "howmany": {
       callback("Je regarde ça...");
-      gSheetsClient.retrieveObject(entity, "stock", callback);
+      gSheetsClient.retrieveObject(entityValues, "stock", callback);
       break;
     }
     case "expiration": {
       callback("Je regarde ça...");
-      gSheetsClient.retrieveObject(entity, "expiration date", callback);
+      gSheetsClient.retrieveObject(entityValues, "expiration date", callback);
       break;
     }
     case "toorder": {
       callback("Je regarde ça...");
-      gSheetsClient.retrieveObject(entity, "to order", callback);
+      gSheetsClient.retrieveObject(entityValues, "to order", callback);
       break;
     }
     case "joke": {
@@ -66,7 +70,7 @@ module.exports.getAnswer = function (body, callback) {
       break;
     }
     case "affirmation": {
-      callback(oneOf(["/shrug", ":)"]));
+      callback(oneOf(["¯\\_(ツ)_/¯", ":)"]));
       break;
     }
     case "congrats": {
@@ -77,10 +81,40 @@ module.exports.getAnswer = function (body, callback) {
       callback("¯\\_(ツ)_/¯");
       break;
     }
+    case "whosthebest":
+      if(entityNames.indexOf(TITANE) !== -1){
+        callback("Je dirais que c'est Motthieu !");
+      } else if(entityNames.indexOf(TOPAZE) !== -1){
+        callback("Facile, c'est Sophie... :) ")
+      } else if(entityNames.indexOf(ACTION) !== -1){
+        callback("On me dit que c'est Cécilia !")
+      } else if(entityNames.indexOf(FORMAT) !== -1){
+        callback("Il me semble que c'est Helena!")
+      } else {
+        callback("Je ne vois pas de qui tu parles...");
+      }
+      break;
     default: {
       callback("Quelqu'un me parle ?")
     }
   }
+};
+
+var getEntityValues = function(entities, query){
+  var entityValues = [];
+  entities.forEach(function(entity){
+    entityValues.push(query.substring(entity.start, entity.end));
+  });
+  return entityValues;
+};
+
+var getEntityNames = function(entities){
+  var entityNames = [];
+  entities.forEach(function(entity){
+    entityNames.push(entity.entity.entityType.name);
+  });
+  console.log(entityNames);
+  return entityNames
 };
 
 var oneOf = function (list) {
